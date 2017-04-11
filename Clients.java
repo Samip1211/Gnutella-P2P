072be1b6e2f5e3b6342af6b93dbaa9e87cf54047
Samip1211 <kothari.samip@gmail.com>
2017-04-11 17:56:58 -0500

    Clients can talk

--- Clients.java
+++ Clients.java
@@ -2,34 +2,37 @@ import java.util.*;
 import java.net.*;
 import java.io.*;
 
-//Object describing the client which is stored in server
-/*
+//Object describing the client which is passed aroundvichar
+
 class ClientRequestAndResponseInformation implements Serializable{
-	int port; // Port of the client
-	int getOtherClient; //0 means do not get other client address whereas 1 means to get other client address
-	List<String> filesPresent;
-	String getFile;
-	public ClientRequestAndResponseInformation(int id){
-		this.port= id;
-		filesPresent = new ArrayList<String>();
-		File folder = new File(System.getProperty("user.dir"));
-		File[] listOfFiles = folder.listFiles();
+	boolean searchQuery;
+	boolean hitQuery;
+	String messageId;
+	int tTl;
+	String fileName;
+	int sourcePort;
+	
+	
+	public void setQuery(int serverId, int tTl,String fileName,String messageId){
+		this.searchQuery=true;
+		this.hitQuery= false;
+		this.messageId= messageId;
+		this.tTl= tTl;
+		this.sourcePort= serverId;
+		this.fileName=fileName;
+	}
+	public void setHitQuery(){
 		
-		//Get the files present in the directory
-		for (int i = 0; i < listOfFiles.length; i++) {
-		      if (listOfFiles[i].isFile()) {
-				  filesPresent.add(listOfFiles[i].getName());
-		        //System.out.println("File " + listOfFiles[i].getName());
-		      } else if (listOfFiles[i].isDirectory()) {
-		        //System.out.println("Directory " + listOfFiles[i].getName());
-		      }
-		    }
 	}
 	
-}*/
+}
+
+
 public class Clients {
 	static int port;
 	static String fileToGet;
+	static List<Integer> otherClients;
+	static Map<String,ClientRequestAndResponseInformation> map; 
 	//Call this method when u want your client to act as server
 	public static void makeServer(int port) {
 		
@@ -48,8 +51,30 @@ public class Clients {
 							//Read the object stream
 							ObjectInputStream in = new ObjectInputStream(threadClient.getInputStream());
 							//Get the file to send to the other client
-							String fileName= (String) in.readObject();
+							ClientRequestAndResponseInformation clientRequestAndResponseInformation= (ClientRequestAndResponseInformation) in.readObject();
 							//Print the file name of the file requested
+							if(map.get(clientRequestAndResponseInformation.messageId)!=null){
+								
+								if(clientRequestAndResponseInformation.searchQuery && clientRequestAndResponseInformation.tTl>0){
+								clientRequestAndResponseInformation.tTl--;
+								
+								map.put(clientRequestAndResponseInformation.messageId,clientRequestAndResponseInformation);
+								
+									for(int otherClientPort:otherClients){
+										connectToClientforFileInformation(otherClientPort,clientRequestAndResponseInformation);
+									}
+								}
+							}else{
+								DataOutputStream out= new DataOutputStream(threadClient.getOutputStream());
+								out.writeUTF("Hey I saw that");
+								
+							}
+							
+							//storeTheObject();
+							//sendTheObjectToOtherClients();
+							
+							
+							/*
 							System.out.println("File NAme" + fileName);
 							//Initalize file object and write in the outputstream
 							File myFile = new File(fileName);
@@ -61,11 +86,11 @@ public class Clients {
 							OutputStream os = threadClient.getOutputStream();
 							os.write(mybytearray, 0, mybytearray.length);
 							os.flush();
-							in.close();
+							in.close();*/
 							//Get the time difference from the initialization of the thread till now
 							long end = System.currentTimeMillis( );
-							 long diff = end - start;
-							  System.out.println("Difference is : " + diff + " "+ start + "  "+ end);
+							long diff = end - start;
+							System.out.println("Difference is : " + diff + " "+ start + "  "+ end);
 				
 						}catch(Exception e){
 							System.out.println(e);
@@ -81,9 +106,28 @@ public class Clients {
 			System.out.println(e);
 		}
 	}
-	
+	public static void connectToClientforFileInformation(int serverPort, ClientRequestAndResponseInformation clientRequestAndResponseInformation){
+		
+		try{
+			Socket client = new Socket("localhost",serverPort);
+		    
+			OutputStream outToServer = client.getOutputStream();
+		    ObjectOutputStream out = new ObjectOutputStream(outToServer);
+			out.writeObject(clientRequestAndResponseInformation);
+			while(true){
+				DataInputStream in = new DataInputStream(client.getInputStream());
+				System.out.println(in.readUTF());
+				
+			}
+			
+			
+		}catch(Exception e){
+			System.out.println(e);
+		}
+		
+	}
 	//Call this method when u want to connect to the other client
-	public  static  void connectToClient(int serverPort){
+	public  static  void connectToClientToDownloadFile(int serverPort){
 		
 		
 		try{
@@ -126,8 +170,8 @@ public class Clients {
 	
 	public static void main(String[] args){
 		//List of other clients
-		List<Integer> otherClients = new ArrayList<Integer>();
-		
+		otherClients = new ArrayList<Integer>();
+		map = new HashMap<String,ClientRequestAndResponseInformation>();
 		//Check the inline input
 		if(args.length>=2){
 			port= Integer.valueOf(args[0]);
@@ -151,6 +195,7 @@ public class Clients {
 		for(int c:otherClients){
 			System.out.println(c);
 		}
+		//Run the server in background
 		
 		new Thread(){
 			public void run(){
@@ -158,17 +203,34 @@ public class Clients {
 				
 			}
 		}.start();
-		String query;
 		
-		//Get the user query
+		String query;
+		int ttl=3;
+		//Get the user query i.e the file name
 		while(true){
 			Scanner reader = new Scanner(System.in);  // Reading from System.in
 			System.out.println("Enter the query: ");
 			query = reader.nextLine();
-			
+			//signal to exit
 			if(query.equals("exit")){
 				break;
+			}else{
+				//Search for the file in the query
+				String uuid = UUID.randomUUID().toString();
+				//System.out.println(uuid); Print the randomly generted string
+				ClientRequestAndResponseInformation clientRequestAndResponseInformation = new ClientRequestAndResponseInformation();
+			
+				for(int otherClientPort:otherClients){
+					//set the object to be send 
+					clientRequestAndResponseInformation.setQuery(port,ttl,query,uuid);
+					//store the object in the map
+					map.put(uuid,clientRequestAndResponseInformation);
+					connectToClientforFileInformation(otherClientPort,clientRequestAndResponseInformation);
+				
+				}
 			}
+			
+			
 		}
 		System.exit(0);
 		
