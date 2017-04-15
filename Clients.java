bcceac5f5cfb32e3824e7af6414b1c06914ede3c
Samip Kothari <samipkothari@Samips-MacBook-Pro.local>
2017-04-15 00:58:30 -0500

    Solved bugsfor 1st part

--- Clients.java
+++ Clients.java
@@ -6,13 +6,13 @@ import java.io.*;
 
 class ClientRequestAndResponseInformation implements Serializable{
 	boolean searchQuery;
+	boolean getFile;
 	boolean hitQuery;
 	String messageId;
 	int tTl;
 	String fileName;
 	int sourcePort;
 	int destPort;
-	
 	public void setQuery(int serverId, int tTl,String fileName,String messageId){
 		this.searchQuery=true;
 		this.hitQuery= false;
@@ -22,8 +22,13 @@ class ClientRequestAndResponseInformation implements Serializable{
 		this.fileName=fileName;
 	}
 	
+	public void setForOtherClient(){
+		
+	}
 	
 }
+
+
 
 
 public class Clients {
@@ -32,7 +37,8 @@ public class Clients {
 	static List<Integer> otherClients;
 	static Map<String,ClientRequestAndResponseInformation> map;
 	static String query;
-	static int ttl=3; 
+	static int ttl=3;
+	static List<String> filesPresent;
 	//Call this method when u want your client to act as server
 	public static void makeServer(int port) {
 		
@@ -46,7 +52,6 @@ public class Clients {
 				new Thread(){
 					public  void run(){
 						try{
-							//Get the current time
 							long start = System.currentTimeMillis();
 							//Read the object stream
 							ObjectInputStream in = new ObjectInputStream(threadClient.getInputStream());
@@ -55,23 +60,34 @@ public class Clients {
 							//Print to console that serving one object
 							System.out.println("Got Object");
 							
-							
 							//Seeing this request for first time
 							if(map.get(clientRequestAndResponseInformation.messageId)==null){
+								System.out.println(clientRequestAndResponseInformation.searchQuery);
 								//See if this query is valid
-								if(clientRequestAndResponseInformation.searchQuery && clientRequestAndResponseInformation.tTl>0){
+								if(clientRequestAndResponseInformation.tTl>0){
 									//descrease the TTL
 									clientRequestAndResponseInformation.tTl--;
 									//Store the query for further use
 									map.put(clientRequestAndResponseInformation.messageId,clientRequestAndResponseInformation);
 									//Send this query to all the other clients connected to the current client
+									
+									
+									
 									new Thread(){
 										public synchronized void run(){
+											
 											for(int otherClientPort:otherClients){
 												//Don't send the query to the client who has started this query
+												ClientRequestAndResponseInformation forOtherClient= new ClientRequestAndResponseInformation();
+												
 												if(clientRequestAndResponseInformation.sourcePort!=otherClientPort){
+													
+													forOtherClient.setQuery(clientRequestAndResponseInformation.sourcePort,clientRequestAndResponseInformation.tTl,
+																		clientRequestAndResponseInformation.fileName,clientRequestAndResponseInformation.messageId);
+													
 													System.out.println("Sending to :"+otherClientPort);
-													forwardTheRequestToOtherClients(otherClientPort,clientRequestAndResponseInformation);
+													
+													forwardTheRequestToOtherClients(otherClientPort,forOtherClient);
 												}else{
 													//Print
 													System.out.println("Not forwarding to"+otherClientPort+"as it is the origin");
@@ -81,29 +97,59 @@ public class Clients {
 										}
 									}.start();
 									
+									checkTheRequestAndSendTheFileIfExist(clientRequestAndResponseInformation);
+								
+								
+									long end = System.currentTimeMillis( );
+									long diff = end - start;
+									//Get the time difference from the initialization of the thread till now
+									System.out.println("Difference to complete request : " + diff + " "+ start + "  "+ end);
+										
 								}
-								
-								checkTheRequestAndSendTheFileIfExist(clientRequestAndResponseInformation);
-								
-								
-								long end = System.currentTimeMillis( );
-								long diff = end - start;
-								//Get the time difference from the initialization of the thread till now
-								System.out.println("Difference is : " + diff + " "+ start + "  "+ end);
 							}else{
 								//Since it is seen request check if it is hit Query.
 								if(clientRequestAndResponseInformation.hitQuery){
 									try{
 										System.out.println("Got the reply from "+clientRequestAndResponseInformation.destPort);
+										
+										connectToClientToDownloadFile(clientRequestAndResponseInformation.destPort,clientRequestAndResponseInformation.fileName,clientRequestAndResponseInformation.messageId);
 									}catch(Exception e){
 										System.out.println(e);
 										System.out.println("There's something wrong with the connection");
 									}
 								}
+								if(clientRequestAndResponseInformation.getFile){
+									
+									
+									//Get the current time
+									long startForFile = System.currentTimeMillis();
+									
+									File myFile = new File(clientRequestAndResponseInformation.fileName);
+									
+									System.out.println(myFile.length());
+									
+									byte[] mybytearray = new byte[(int) myFile.length()];
+									
+									BufferedInputStream bis = new BufferedInputStream(new FileInputStream(myFile));
+									
+									bis.read(mybytearray, 0, mybytearray.length);
+							
+									OutputStream os = threadClient.getOutputStream();
+									
+									os.write(mybytearray, 0, mybytearray.length);
+									
+									os.close();
+									long end = System.currentTimeMillis( );
+									long diff = end - startForFile;
+									
+									System.out.println("Difference to send file : " + diff);
+																
+								}
 									//System.out.println("Already seen this query and took neccessary actions. No need to do now");					
 								
 							}
-							
+								
+								
 						}catch(Exception e){
 							System.out.println(e);
 						}
@@ -121,10 +167,10 @@ public class Clients {
 		//LogOut the fileName
 		System.out.println(clientRequestAndResponseInformation.fileName);
 		//See if the file is present.
-		if(clientRequestAndResponseInformation.fileName.equals("abc")){
+		if(filesPresent.contains(clientRequestAndResponseInformation.fileName)){
 			//Set the query so as to reply.
 			clientRequestAndResponseInformation.hitQuery=true;
-			clientRequestAndResponseInformation.searchQuery=false;
+			
 			clientRequestAndResponseInformation.destPort=port;
 			//Connect to that client
 			System.out.println("Trying to connect to"+clientRequestAndResponseInformation.sourcePort );
@@ -134,7 +180,7 @@ public class Clients {
 			
 				ObjectOutputStream out= new ObjectOutputStream(client.getOutputStream());
 				out.writeObject(clientRequestAndResponseInformation);
-			
+				out.flush();
 				out.close();	
 			}catch(Exception e){
 				System.out.println(e);
@@ -184,48 +230,79 @@ public class Clients {
 		}
 		
 	}
+	
 	//Call this method when u want to connect to the other client
-	public  static  void connectToClientToDownloadFile(int serverPort){
+	public  static synchronized void connectToClientToDownloadFile(int serverPort,String fileName,String messageId){
 		
 		
 		try{
-			//Connect to other client
-			Socket client = new Socket("localhost", serverPort);
-			//Start the time
-			long start = System.currentTimeMillis();
-			//Get the output stream
-		    OutputStream outToServer = client.getOutputStream();
-		    ObjectOutputStream out = new ObjectOutputStream(outToServer);
+			if(!filesPresent.contains(fileName))
+			{
+				//Initailize new object to send
+				ClientRequestAndResponseInformation sendObjectForFile= new ClientRequestAndResponseInformation();
+				sendObjectForFile.getFile=true;
+				sendObjectForFile.hitQuery=false;
+				sendObjectForFile.fileName=fileName;
+				sendObjectForFile.messageId=messageId;
+				
+				System.out.println("Going to get the file from "+ serverPort);
+				
+				//Connect to other client
+				Socket client = new Socket("localhost", serverPort);
+				//Start the time
+				long start = System.currentTimeMillis();
+				//Get the output stream
+			    OutputStream outToServer = client.getOutputStream();
+			    ObjectOutputStream out = new ObjectOutputStream(outToServer);
          
-		   	//Print the file name of the file which is being requested and savre the file
-			System.out.println("File to Get" + fileToGet);
-			out.writeObject(fileToGet);
+			   	//Print the file name of the file which is being requested and savre the file
+				System.out.println("File to Get"+ sendObjectForFile.fileName );
+				out.writeObject(sendObjectForFile);
 			
-			byte[] mybytearray = new byte[6022386];
-			InputStream is = client.getInputStream();
-			FileOutputStream fos = new FileOutputStream(fileToGet);
-		    int bytesRead;
-		    int current = 0;
-			BufferedOutputStream bos = null;
-			bos = new BufferedOutputStream(fos);
-			bytesRead = is.read(mybytearray,0,mybytearray.length);
-			do {
-			        bos.write(mybytearray);
-			        bytesRead = is.read(mybytearray);
-			 } while (bytesRead != -1);
+				byte[] mybytearray = new byte[6022386];
+				InputStream is = client.getInputStream();
+				FileOutputStream fos = new FileOutputStream(fileName);
+			    int bytesRead;
+			    int current = 0;
+				BufferedOutputStream bos = null;
+				bos = new BufferedOutputStream(fos);
+				bytesRead = is.read(mybytearray,0,mybytearray.length);
+				do {
+				        bos.write(mybytearray);
+				        bytesRead = is.read(mybytearray);
+				 } while (bytesRead != -1);
 			
-			bos.flush();
+				bos.flush();
 			
-			client.close();
-			//Get the difference between the connection of other client and getting the file
-			long end = System.currentTimeMillis( );
-			long diff = end - start;
-			System.out.println("Difference is : " + diff);
+				client.close();
+				//Get the difference between the connection of other client and getting the file
+				long end = System.currentTimeMillis( );
+				long diff = end - start;
+				System.out.println("Difference to get file : " + diff);
+				filesPresent.add(fileName);
+			}
+			
 		}catch(Exception e){
 			System.out.println(e);
 		}
 	}
-	
+	 static void saveFilesinCurrDir(){
+		filesPresent = new ArrayList<String>();
+		
+		File folder = new File(System.getProperty("user.dir"));
+		
+		File[] listOfFiles = folder.listFiles();
+		
+		//Get the files present in the directory
+		for (int i = 0; i < listOfFiles.length; i++) {
+			if (listOfFiles[i].isFile()) {
+				filesPresent.add(listOfFiles[i].getName());
+				        //System.out.println("File " + listOfFiles[i].getName());
+			} else if (listOfFiles[i].isDirectory()) {
+				        //System.out.println("Directory " + listOfFiles[i].getName());
+			}
+		}
+	}
 	public static void main(String[] args){
 		//List of other clients
 		otherClients = new ArrayList<Integer>();
@@ -249,6 +326,8 @@ public class Clients {
 			System.out.println("Please enter the valid configFile");
 			return;
 		}
+		
+		saveFilesinCurrDir();
 		//Print the  nearby clients
 		for(int c:otherClients){
 			System.out.println(c);
